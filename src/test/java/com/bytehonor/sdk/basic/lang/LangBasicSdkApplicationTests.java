d3f217e63f861ef93bdf422345010998aba4bf6a
lijianqiang <bytehonor@126.com>
2019-12-02 20:05:39 +0800

    init

---
+++ src/test/java/com/bytehonor/sdk/basic/lang/LangBasicSdkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bytehonor.sdk.basic.lang;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LangBasicSdkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
