d60d9f87a1d2be26010d6982dd4c468688d9e94e
lijianqiang <bytehonor@126.com>
2020-10-12 19:13:05 +0800

    0.2.4

--- src/main/java/com/bytehonor/sdk/basic/lang/util/LongIdUtils.java
+++ src/main/java/com/bytehonor/sdk/basic/lang/util/LongIdUtils.java
@@ -2,15 +2,47 @@ package com.bytehonor.sdk.basic.lang.util;
 
 public class LongIdUtils {
 
-    public static final long MAX = 999999999999L; // 12bits
+    public static final long MAX = 999987654321L; // 12bits
 
+    private static final int SCALE_62 = 62;
+
+    private static final String CHAR_STRING = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    private static final char[] CHARS = CHAR_STRING.toCharArray();
+
+    // 数字转62进制
     public static String encode(long num) {
-        long val = MAX - num;
-        return Long.toString(val, 36);
+        num = MAX - num;
+        StringBuilder sb = new StringBuilder();
+        int remainder;
+        int limit = SCALE_62 - 1;
+        while (num > limit) {
+            // 对 scale 进行求余，然后将余数追加至 sb 中，由于是从末位开始追加的，因此最后需要反转字符串
+            remainder = Long.valueOf(num % SCALE_62).intValue();
+            sb.append(CHARS[remainder]);
+            // 除以进制数，获取下一个末尾数
+            num = num / SCALE_62;
+        }
+        sb.append(CHARS[Long.valueOf(num).intValue()]);
+        return sb.reverse().toString();
     }
 
-    public static long decode(String src) {
-        long val = Long.valueOf(src, 36);
-        return MAX - val;
+    // 62进制转为数字
+    public static long decode(String str) {
+        // 将 0 开头的字符串进行替换
+        str = str.replace("^0*", "");
+        long value = 0;
+        char tempChar;
+        int tempCharValue;
+        int length = str.length();
+        for (int i = 0; i < length; i++) {
+            // 获取字符
+            tempChar = str.charAt(i);
+            // 单字符值
+            tempCharValue = CHAR_STRING.indexOf(tempChar);
+            // 单字符值在进制规则下表示的值
+            value += (long) (tempCharValue * Math.pow(SCALE_62, length - i - 1));
+        }
+        return MAX - value;
     }
 }
--- src/test/java/com/bytehonor/sdk/basic/lang/util/LongIdUtilsTest.java
+++ src/test/java/com/bytehonor/sdk/basic/lang/util/LongIdUtilsTest.java
@@ -3,31 +3,8 @@ package com.bytehonor.sdk.basic.lang.util;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class LongIdUtilsTest {
-
-    private static final Logger LOG = LoggerFactory.getLogger(LongIdUtilsTest.class);
-
-    private static long num = 9;
-
-    private static String ENC = "cre66i9i";
-
-    @Test
-    public void testEncode() {
-        String src = LongIdUtils.encode(num);
-        LOG.info("src:{}", src);
-        assertTrue("*testEncode*", ENC.equals(src));
-
-    }
-
-    @Test
-    public void testDecode() {
-        long val = LongIdUtils.decode(ENC);
-        LOG.info("val:{}", val);
-        assertTrue("*testDecode*", val == num);
-    }
 
     @Test
     public void test() {
