ba7109b989f3cca3cc80dac2a9210501b6c61fe5
lijianqiang <343891954@qq.com>
2021-07-04 17:35:11 +0800

    1.0.0

--- src/main/java/com/bytehonor/sdk/lang/bytehonor/string/StringObject.java
+++ src/main/java/com/bytehonor/sdk/lang/bytehonor/string/StringObject.java
@@ -1,9 +1,13 @@
 package com.bytehonor.sdk.lang.bytehonor.string;
 
+/**
+ * @author lijianqiang
+ *
+ */
 public class StringObject {
 
     public static boolean isEmpty(String str) {
-        return (str == null || "".equals(str));
+        return (str == null || str.isEmpty());
     }
 
     public static boolean equals(String src1, String src2) {
