e7ab0a77009b4ff2fdb893fff4e0ce418e9a5013
lijianqiang <343891954@qq.com>
2021-11-17 22:53:45 +0800

    1.2.6

--- src/main/java/com/bytehonor/sdk/lang/bytehonor/util/JacksonUtils.java
+++ src/main/java/com/bytehonor/sdk/lang/bytehonor/util/JacksonUtils.java
@@ -5,9 +5,10 @@ import java.util.Objects;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.bytehonor.sdk.lang.bytehonor.exception.BytehonorLangException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class JacksonUtils {
@@ -26,8 +27,7 @@ public class JacksonUtils {
         try {
             return JACKSON_MAPPER.readValue(json, valueType);
         } catch (Exception e) {
-            LOG.error("json:{}, error", json, e);
-            throw new RuntimeException(e);
+            throw new BytehonorLangException("readValue valueType error", e);
         }
     }
 
@@ -37,8 +37,7 @@ public class JacksonUtils {
         try {
             return JACKSON_MAPPER.readValue(json, valueTypeRef);
         } catch (Exception e) {
-            LOG.error("json:{}, error", json, e);
-            throw new RuntimeException(e);
+            throw new BytehonorLangException("readValue valueTypeRef error", e);
         }
     }
 
@@ -46,9 +45,43 @@ public class JacksonUtils {
         Objects.requireNonNull(value, "value");
         try {
             return JACKSON_MAPPER.writeValueAsString(value);
-        } catch (JsonProcessingException e) {
-            LOG.error("toJson", e);
-            throw new RuntimeException(e);
+        } catch (Exception e) {
+            throw new BytehonorLangException("writeValueAsString error", e);
         }
+    }
+
+    public static JsonNode readTree(String json) {
+        Objects.requireNonNull(json, "json");
+        try {
+            return JACKSON_MAPPER.readTree(json);
+        } catch (Exception e) {
+            throw new BytehonorLangException("readTree error", e);
+        }
+    }
+
+    public static String get(JsonNode jn, String key) {
+        if (jn == null) {
+            LOG.warn("jn null, get key:{}", key);
+            return null;
+        }
+        return jn.has(key) ? jn.get(key).asText() : null;
+    }
+
+    public static String get(JsonNode jn, int index) {
+        if (jn == null) {
+            LOG.warn("jn null, get index:{}", index);
+            return null;
+        }
+        return jn.has(index) ? jn.get(index).asText() : null;
+    }
+
+    public static JsonNode nodeRequired(JsonNode jn, String field) {
+        Objects.requireNonNull(jn, "jn");
+        Objects.requireNonNull(field, "field");
+        JsonNode node = jn.get(field);
+        if (node == null) {
+            throw new BytehonorLangException("JsonNode null, field:" + field);
+        }
+        return node;
     }
 }
