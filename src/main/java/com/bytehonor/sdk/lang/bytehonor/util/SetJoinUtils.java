fa6db2340dda1249000177f8982eadd65a73a60f
lijianqiang <343891954@qq.com>
2021-11-17 22:06:24 +0800

    1.2.4

--- src/main/java/com/bytehonor/sdk/lang/bytehonor/util/SetJoinUtils.java
+++ src/main/java/com/bytehonor/sdk/lang/bytehonor/util/SetJoinUtils.java
@@ -9,51 +9,51 @@ public class SetJoinUtils {
     /**
      * with quote ''
      * 
-     * @param list
+     * @param set
      * @return
      */
-    public static String joinStringSafe(Set<String> list) {
-        if (list == null || list.isEmpty()) {
+    public static String joinStringSafe(Set<String> set) {
+        if (set == null || set.isEmpty()) {
             return "";
         }
         StringBuilder sb = new StringBuilder();
-        for (String val : list) {
+        for (String val : set) {
             sb.append("'").append(val).append("',");
         }
         String str = sb.toString();
         return str.substring(0, str.length() - 1);
     }
 
-    public static String joinString(Set<String> list) {
-        if (list == null || list.isEmpty()) {
+    public static String joinString(Set<String> set) {
+        if (set == null || set.isEmpty()) {
             return "";
         }
         StringBuilder sb = new StringBuilder();
-        for (String val : list) {
+        for (String val : set) {
             sb.append(val).append(",");
         }
         String str = sb.toString();
         return str.substring(0, str.length() - 1);
     }
 
-    public static String joinLong(Set<Long> list) {
-        if (list == null || list.isEmpty()) {
+    public static String joinLong(Set<Long> set) {
+        if (set == null || set.isEmpty()) {
             return "";
         }
         StringBuilder sb = new StringBuilder();
-        for (Long val : list) {
+        for (Long val : set) {
             sb.append(val).append(CON);
         }
         String str = sb.toString();
         return str.substring(0, str.length() - 1);
     }
 
-    public static String joinInteger(Set<Integer> list) {
-        if (list == null || list.isEmpty()) {
+    public static String joinInteger(Set<Integer> set) {
+        if (set == null || set.isEmpty()) {
             return "";
         }
         StringBuilder sb = new StringBuilder();
-        for (Integer val : list) {
+        for (Integer val : set) {
             sb.append(val).append(CON);
         }
         String str = sb.toString();
