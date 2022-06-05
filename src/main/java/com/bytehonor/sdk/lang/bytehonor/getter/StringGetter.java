73c500be6f1960bb0a960766cc59a567a66b69a2
lijianqiang <343891954@qq.com>
2022-06-05 22:07:13 +0800

    1.3.20

--- src/main/java/com/bytehonor/sdk/lang/bytehonor/getter/StringGetter.java
+++ src/main/java/com/bytehonor/sdk/lang/bytehonor/getter/StringGetter.java
@@ -1,5 +1,7 @@
 package com.bytehonor.sdk.lang.bytehonor.getter;
 
+import com.bytehonor.sdk.define.bytehonor.util.StringObject;
+
 /**
  * @author lijianqiang
  *
@@ -8,6 +10,10 @@ public class StringGetter {
 
     public static String optional(String src, String def) {
         return src != null ? src : def;
+    }
+
+    public static String getOrNull(String src) {
+        return StringObject.isEmpty(src) ? null : src;
     }
 
 }
