6973d7e7fb2a4fe43d86b09b2d948e850356c843
lijianqiang <newphper@126.com>
2022-06-21 15:39:57 +0800

    1.3.23

---
+++ src/main/java/com/bytehonor/sdk/lang/bytehonor/task/ScheduleTaskExecutor.java
@@ -0,0 +1,17 @@
+package com.bytehonor.sdk.lang.bytehonor.task;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import com.bytehonor.sdk.define.bytehonor.task.SafeTask;
+
+public class ScheduleTaskExecutor {
+
+    private static final ScheduledExecutorService SERVICE = Executors.newSingleThreadScheduledExecutor();
+
+    public static void schedule(SafeTask command, long delaySeconds, long periodSeconds) {
+        // 第二个参数为首次执行的延时时间，第三个参数为定时执行的间隔时间
+        SERVICE.scheduleAtFixedRate(command, delaySeconds, periodSeconds, TimeUnit.SECONDS);
+    }
+}
