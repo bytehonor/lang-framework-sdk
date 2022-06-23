86541ef15f52285db93eb33e9f71d2f2f5253839
lijianqiang <newphper@126.com>
2022-06-23 16:18:41 +0800

    0.1.1

--- src/main/java/com/bytehonor/sdk/lang/spring/function/ClassGetter.java
+++
@@ -1,8 +0,0 @@
-package com.bytehonor.sdk.lang.spring.function;
-
-import java.io.Serializable;
-import java.util.function.Function;
-
-@FunctionalInterface
-public interface ClassGetter<T, R> extends Function<T, R>, Serializable {
-}
