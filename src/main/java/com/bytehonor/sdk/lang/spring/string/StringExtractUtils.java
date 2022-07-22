eabb48b1b9de9d3efe46e563815d4121f133fb8a
lijianqiang <343891954@qq.com>
2022-07-22 22:14:59 +0800

    0.2.7

--- src/main/java/com/bytehonor/sdk/lang/spring/string/StringExtractUtils.java
+++ src/main/java/com/bytehonor/sdk/lang/spring/string/StringExtractUtils.java
@@ -27,110 +27,108 @@ public class StringExtractUtils {
     /**
      * <pre>
      * 截取一段, 一定是从开头到结尾, 即使先查到结尾
-     * 
-     * 前后保留 beginner 和 ender
-     * 
+     * 前后保留 header 和 footer
+     * 支持*号做模糊匹配, 如he*der
      * </pre>
      * 
      * @param src
-     * @param beginner
-     * @param ender
+     * @param header
+     * @param footer
      * @return
      */
-    public static String extract(String src, String beginner, String ender) {
-        return doExtract(src, beginner, ender, false);
+    public static String extract(String src, String header, String footer) {
+        return doExtract(src, header, footer, false);
     }
 
     /**
      * <pre>
      * 截取一段, 一定是从开头到结尾, 即使先查到结尾
-     * 
-     * 前后剔除 beginner 和 ender
-     * 
+     * 前后剔除 header 和 footer
+     * 支持*号做模糊匹配, 如he*der
      * </pre>
      * 
      * @param src
-     * @param beginner
-     * @param ender
+     * @param header
+     * @param footer
      * @return
      */
-    public static String extractTrim(String src, String beginner, String ender) {
-        return doExtract(src, beginner, ender, true);
+    public static String extractTrim(String src, String header, String footer) {
+        return doExtract(src, header, footer, true);
     }
 
-    private static String doExtract(String src, String beginner, String ender, boolean trim) {
+    private static String doExtract(String src, String header, String footer, boolean trim) {
         Objects.requireNonNull(src, "src");
-        Objects.requireNonNull(beginner, "beginner");
-        Objects.requireNonNull(ender, "ender");
+        Objects.requireNonNull(header, "header");
+        Objects.requireNonNull(footer, "footer");
 
         if (StringObject.isEmpty(src)) {
             return EMPTY;
         }
-        if (StringObject.isEmpty(beginner) || StringObject.isEmpty(ender)) {
-            LOG.warn("beginner:{} or ender:{} empty");
+        if (StringObject.isEmpty(header) || StringObject.isEmpty(footer)) {
+            LOG.warn("header:{} or footer:{} empty");
             return EMPTY;
         }
         final int length = src.length();
         final char[] source = src.toCharArray();
-        final int beginLength = beginner.length();
-        final char[] beginChar = beginner.toCharArray();
-        final int endLenght = ender.length();
-        final char[] endChar = ender.toCharArray();
+        final int headerSize = header.length();
+        final char[] headers = header.toCharArray();
+        final int footerSize = footer.length();
+        final char[] footers = footer.toCharArray();
 
-        boolean findBegin = false;
+        boolean findheader = false;
         int beginAt = 0;
         int i = 0;
         int j = 0;
         int k = 0;
         for (i = 0; i < length; i++) {
-            if (source[i] != beginChar[0]) {
+            if (source[i] != headers[0]) {
                 continue;
             }
-            for (j = 0, k = i + j; j < beginLength && k < length; j++, k++) {
-                if (IGNORE_CHAR == beginChar[j]) {
+            for (j = 0, k = i + j; j < headerSize && k < length; j++, k++) {
+                if (IGNORE_CHAR == headers[j]) {
                     continue;
                 }
-                if (source[k] != beginChar[j]) {
+                if (source[k] != headers[j]) {
                     break;
                 }
             }
-            if (j == beginLength) {
-                findBegin = true;
+            if (j == headerSize) {
+                findheader = true;
                 beginAt = i;
                 break;
             }
         }
 
-        LOG.debug("findBegin:{}, beginAt:{}, i:{}", findBegin, beginAt, i);
-        if (findBegin == false) {
+        LOG.debug("findheader:{}, beginAt:{}, i:{}", findheader, beginAt, i);
+        if (findheader == false) {
             return EMPTY;
         }
 
-        boolean findEnd = false;
+        boolean findFooter = false;
         int count = 0;
         for (; i < length; i++) {
             count++;
-            if (source[i] != endChar[0]) {
+            if (source[i] != footers[0]) {
                 continue;
             }
-            for (j = 0, k = i + j; j < endLenght && k < length; j++, k++) {
-                if (IGNORE_CHAR == endChar[j]) {
+            for (j = 0, k = i + j; j < footerSize && k < length; j++, k++) {
+                if (IGNORE_CHAR == footers[j]) {
                     continue;
                 }
-                if (source[k] != endChar[j]) {
+                if (source[k] != footers[j]) {
                     break;
                 }
             }
-            if (j == endLenght) {
-                count += (endLenght - 1);
-                findEnd = true;
+            if (j == footerSize) {
+                count += (footerSize - 1);
+                findFooter = true;
                 break;
             }
         }
-        if (findBegin && findEnd) {
+        if (findheader && findFooter) {
             if (trim) {
-                beginAt = beginAt + beginLength;
-                count = count - endLenght - beginLength;
+                beginAt = beginAt + headerSize;
+                count = count - footerSize - headerSize;
             }
             // LOG.debug("beginAt:{}, count:{}", beginAt, count);
             return new String(source, beginAt, count);
