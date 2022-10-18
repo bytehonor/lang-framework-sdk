7e014a2b04bc5181efe664c140dcbe451fab26fa
lijianqiang <newphper@126.com>
2022-10-18 10:01:27 +0800

    0.3.20

--- src/main/java/com/bytehonor/sdk/lang/spring/meta/MetaModel.java
+++ src/main/java/com/bytehonor/sdk/lang/spring/meta/MetaModel.java
@@ -1,8 +1,11 @@
 package com.bytehonor.sdk.lang.spring.meta;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 import org.springframework.util.CollectionUtils;
@@ -19,14 +22,17 @@ public class MetaModel {
 
     private List<MetaModelField> fields;
 
-    private Set<String> keys;
+    private final Set<String> keys;
 
-    private Set<String> columns;
+    private final Set<String> columns;
+
+    private final Map<String, MetaModelField> map;
 
     public MetaModel() {
         fields = new ArrayList<MetaModelField>();
         keys = new HashSet<String>();
         columns = new HashSet<String>();
+        map = new HashMap<String, MetaModelField>();
     }
 
     public String getName() {
@@ -49,33 +55,46 @@ public class MetaModel {
         return keys;
     }
 
-    public void setKeys(Set<String> keys) {
-        this.keys = keys;
-    }
-
     public Set<String> getColumns() {
         return columns;
-    }
-
-    public void setColumns(Set<String> columns) {
-        this.columns = columns;
     }
 
     public void check() {
         if (CollectionUtils.isEmpty(fields)) {
             throw new SpringLangException("no fields");
         }
+
         for (MetaModelField field : fields) {
-            this.keys.add(field.getKey());
-            this.columns.add(field.getColumn());
+            keys.add(field.getKey());
+            columns.add(field.getColumn());
+
+            if (map.containsKey(field.getKey()) == false) {
+                map.put(field.getKey(), field);
+            }
+
+            if (map.containsKey(field.getColumn()) == false) {
+                map.put(field.getColumn(), field);
+            }
         }
     }
 
     public boolean hasKey(String key) {
+        Objects.requireNonNull(key, "key");
         return keys.contains(key);
     }
 
     public boolean hasColumn(String column) {
+        Objects.requireNonNull(column, "column");
         return columns.contains(column);
+    }
+
+    public boolean contains(String field) {
+        Objects.requireNonNull(field, "field");
+        return keys.contains(field) || columns.contains(field);
+    }
+
+    public MetaModelField getIfPresent(String field) {
+        Objects.requireNonNull(field, "field");
+        return map.get(field);
     }
 }
