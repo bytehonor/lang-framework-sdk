f0628289dbd078691c9a2df379a2430c1ba59bf7
lijianqiang <newphper@126.com>
2022-12-20 16:29:42 +0800

    0.4.4

--- src/main/java/com/bytehonor/sdk/lang/spring/string/StringBase64Utils.java
+++ src/main/java/com/bytehonor/sdk/lang/spring/string/StringBase64Utils.java
@@ -8,22 +8,34 @@ public class StringBase64Utils {
 
     public static String encodeSafe(String src) {
         Objects.requireNonNull(src, "src");
+        if (SpringString.isEmpty(src)) {
+            return "";
+        }
         return encode(src).replaceAll("=", "~");
     }
 
     public static String decodeSafe(String base) {
         Objects.requireNonNull(base, "base");
+        if (SpringString.isEmpty(base)) {
+            return "";
+        }
         base = base.replaceAll("~", "=");
         return decode(base);
     }
 
     public static String encode(String src) {
         Objects.requireNonNull(src, "src");
+        if (SpringString.isEmpty(src)) {
+            return "";
+        }
         return Base64Utils.encodeToUrlSafeString(src.getBytes());
     }
 
     public static String decode(String base) {
         Objects.requireNonNull(base, "base");
+        if (SpringString.isEmpty(base)) {
+            return "";
+        }
         return new String(Base64Utils.decodeFromUrlSafeString(base));
     }
 }
