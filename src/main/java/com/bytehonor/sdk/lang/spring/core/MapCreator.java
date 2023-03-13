288f5f92af3e244edb2256a74232cc704523d28d
lijianqiang <newphper@126.com>
2023-03-13 12:09:48 +0800

    0.4.6

--- src/main/java/com/bytehonor/sdk/lang/spring/core/MapCreator.java
+++ src/main/java/com/bytehonor/sdk/lang/spring/core/MapCreator.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
+import com.bytehonor.sdk.lang.spring.string.SpringString;
+
 public class MapCreator {
 
     private final Map<String, String> map;
@@ -32,9 +34,25 @@ public class MapCreator {
         return this;
     }
 
-    public MapCreator put(String key, String value) {
+    public MapCreator put(String key, Boolean value) {
         Objects.requireNonNull(key, "key");
         if (value != null) {
+            this.map.put(key, value.toString());
+        }
+        return this;
+    }
+
+    public MapCreator put(String key, Double value) {
+        Objects.requireNonNull(key, "key");
+        if (value != null) {
+            this.map.put(key, value.toString());
+        }
+        return this;
+    }
+
+    public MapCreator put(String key, String value) {
+        Objects.requireNonNull(key, "key");
+        if (SpringString.isEmpty(value) == false) {
             this.map.put(key, value);
         }
         return this;
