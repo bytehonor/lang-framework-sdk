14588b86c18e20f43b1e5696d1119171ac42d830
lijianqiang <newphper@126.com>
2024-12-11 12:08:59 +0800

    final

--- src/main/java/com/bytehonor/sdk/lang/spring/constant/TimeConstants.java
+++ src/main/java/com/bytehonor/sdk/lang/spring/constant/TimeConstants.java
@@ -6,23 +6,23 @@ package com.bytehonor.sdk.lang.spring.constant;
  */
 public class TimeConstants {
 
-    public static long SECOND = 1000L;
+    public static final long SECOND = 1000L;
 
-    public static long MINUTE = SECOND * 60;
+    public static final long MINUTE = SECOND * 60;
 
-    public static long HOUR = MINUTE * 60;
+    public static final long HOUR = MINUTE * 60;
 
-    public static long HOUR_HALF = MINUTE * 30;
+    public static final long HOUR_HALF = MINUTE * 30;
 
-    public static long DAY = HOUR * 24;
+    public static final long DAY = HOUR * 24;
 
-    public static long DAY_HALF = DAY * 12;
+    public static final long DAY_HALF = DAY * 12;
 
-    public static long WEEK = DAY * 7;
+    public static final long WEEK = DAY * 7;
 
-    public static long MONTH = DAY * 30;
+    public static final long MONTH = DAY * 30;
 
-    public static long MONTH_HALF = DAY * 15;
+    public static final long MONTH_HALF = DAY * 15;
 
-    public static long YEAR_HALF = DAY * 180;
+    public static final long YEAR_HALF = DAY * 180;
 }
