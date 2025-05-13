e1ccc159c218017f5c1c5ddcbc45338d16f8fdb4
lijianqiang <newphper@126.com>
2025-05-13 11:30:17 +0800

    clear

--- src/main/java/com/bytehonor/sdk/lang/spring/Java.java
+++ src/main/java/com/bytehonor/sdk/lang/spring/Java.java
@@ -48,66 +48,14 @@ public final class Java {
         return Objects.requireNonNull(obj);
     }
 
-    /**
-     * Checks that the specified object reference is not {@code null} and throws a
-     * customized {@link NullPointerException} if it is. This method is designed
-     * primarily for doing parameter validation in methods and constructors with
-     * multiple parameters, as demonstrated below: <blockquote>
-     * 
-     * <pre>
-     * public Foo(Bar bar, Baz baz) {
-     *     this.bar = Objects.requireNonNull(bar, "bar must not be null");
-     *     this.baz = Objects.requireNonNull(baz, "baz must not be null");
-     * }
-     * </pre>
-     * 
-     * </blockquote>
-     *
-     * @param obj     the object reference to check for nullity
-     * @param message detail message to be used in the event that a {@code
-     *                NullPointerException} is thrown
-     * @param <T>     the type of the reference
-     * @return {@code obj} if not {@code null}
-     * @throws NullPointerException if {@code obj} is {@code null}
-     */
     public static <T> T requireNonNull(T obj, String message) {
         return Objects.requireNonNull(obj, message);
     }
 
-    /**
-     * Returns {@code true} if the provided reference is {@code null} otherwise
-     * returns {@code false}.
-     *
-     * @apiNote This method exists to be used as a
-     *          {@link java.util.function.Predicate},
-     *          {@code filter(Objects::isNull)}
-     *
-     * @param obj a reference to be checked against {@code null}
-     * @return {@code true} if the provided reference is {@code null} otherwise
-     *         {@code false}
-     *
-     * @see java.util.function.Predicate
-     * @since 1.8
-     */
     public static boolean isNull(Object obj) {
         return obj == null;
     }
 
-    /**
-     * Returns {@code true} if the provided reference is non-{@code null} otherwise
-     * returns {@code false}.
-     *
-     * @apiNote This method exists to be used as a
-     *          {@link java.util.function.Predicate},
-     *          {@code filter(Objects::nonNull)}
-     *
-     * @param obj a reference to be checked against {@code null}
-     * @return {@code true} if the provided reference is non-{@code null} otherwise
-     *         {@code false}
-     *
-     * @see java.util.function.Predicate
-     * @since 1.8
-     */
     public static boolean nonNull(Object obj) {
         return obj != null;
     }
